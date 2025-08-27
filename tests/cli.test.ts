07b6bf226b048ac75d4ade6e11231a8b8a650ac5
Chase Adams <c@cadams.io>
2025-08-27 11:17:15 -0700

    fix: test

--- tests/cli.test.ts
+++ tests/cli.test.ts
@@ -1297,6 +1297,12 @@ describe("getConfigFromFile", () => {
                 "serverName": "bigint_number",
                 "type": "number",
               },
+              "boolArray": {
+                "customType": null,
+                "optional": false,
+                "serverName": "bool_array",
+                "type": "json",
+              },
               "booleanField": {
                 "customType": null,
                 "optional": false,
@@ -1332,10 +1338,22 @@ describe("getConfigFromFile", () => {
                 "serverName": "double_precision",
                 "type": "number",
               },
+              "enumArray": {
+                "customType": null,
+                "optional": false,
+                "serverName": "enum_array",
+                "type": "json",
+              },
               "id": {
                 "customType": null,
                 "optional": false,
                 "type": "string",
+              },
+              "intArray": {
+                "customType": null,
+                "optional": false,
+                "serverName": "int_array",
+                "type": "json",
               },
               "integerField": {
                 "customType": null,
@@ -1349,10 +1367,27 @@ describe("getConfigFromFile", () => {
                 "serverName": "json",
                 "type": "json",
               },
+              "jsonbArray": {
+                "customType": null,
+                "optional": false,
+                "serverName": "jsonb_array",
+                "type": "json",
+              },
               "jsonbField": {
                 "customType": null,
                 "optional": false,
                 "serverName": "jsonb",
+                "type": "json",
+              },
+              "matrix": {
+                "customType": null,
+                "optional": false,
+                "type": "json",
+              },
+              "numericArray": {
+                "customType": null,
+                "optional": false,
+                "serverName": "numeric_array",
                 "type": "json",
               },
               "numericField": {
@@ -1468,6 +1503,12 @@ describe("getConfigFromFile", () => {
                 "optional": false,
                 "type": "string",
               },
+              "textArray": {
+                "customType": null,
+                "optional": false,
+                "serverName": "text_array",
+                "type": "json",
+              },
               "textField": {
                 "customType": null,
                 "optional": false,
@@ -1508,6 +1549,12 @@ describe("getConfigFromFile", () => {
                 "customType": null,
                 "optional": true,
                 "type": "number",
+              },
+              "uuidArray": {
+                "customType": null,
+                "optional": false,
+                "serverName": "uuid_array",
+                "type": "json",
               },
               "uuidField": {
                 "customType": null,
